5ecbc5181b13f2f7dff854ba6714455ebf970e5f
DavidDSaputra <davidsaputra2704@gmail.com>
2026-04-13 14:23:56 +0700

    feat: add core theme and constants

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  static const String baseUrl = 'https://YOUR_BACKEND_URL/v1';
+  static const String verifyToken = '/auth/verify-token';
+  static const String products = '/products';
+  static const int connectTimeout = 15000;
+  static const int receiveTimeout = 15000;
+}

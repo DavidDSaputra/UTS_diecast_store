e633ee331e8be0f3bb6b14aa490b467c3994c008
DavidDSaputra <davidsaputra2704@gmail.com>
2026-04-13 16:47:02 +0700

    feat: Inisialisasi Proyek & Konfigurasi Firebase (STEP 1-2)
    - Tambahkan Firebase options untuk semua platform (Android, iOS, macOS, Web, Windows, Linux)
    - Setup pubspec.yaml dengan dependencies (Firebase, Provider, Dio, Secure Storage)
    - Generate native platform folders dengan Flutter CREATE
    - Konfigurasi Google Sign-In client ID di web/index.html
    - Tambahkan project analysis options dan test infrastructure

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,13 +1,75 @@
-// TODO: Replace this placeholder with your generated Firebase configuration.
-// If you have already run `flutterfire configure`, copy the generated
-// `firebase_options.dart` content into this file.
-
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
-    throw UnimplementedError(
-      'Replace this placeholder with your generated Firebase options.',
-    );
+    if (kIsWeb) {
+      return web;
+    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.iOS:
+        return ios;
+      case TargetPlatform.macOS:
+        return macos;
+      case TargetPlatform.windows:
+        return windows;
+      case TargetPlatform.linux:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for linux - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      default:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions are not supported for this platform.',
+        );
+    }
   }
+
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyAuu5qjZZ9BUah73ihiKfZj8pwKRCElOlQ',
+    appId: '1:1053962346056:web:1234567890abcdef',
+    messagingSenderId: '1053962346056',
+    projectId: 'myprojectfirebas-86a91',
+    authDomain: 'myprojectfirebas-86a91.firebaseapp.com',
+    storageBucket: 'myprojectfirebas-86a91.firebasestorage.app',
+  );
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyAuu5qjZZ9BUah73ihiKfZj8pwKRCElOlQ',
+    appId: '1:1053962346056:android:a0a96923cbacaa65a6827d',
+    messagingSenderId: '1053962346056',
+    projectId: 'myprojectfirebas-86a91',
+    storageBucket: 'myprojectfirebas-86a91.firebasestorage.app',
+  );
+
+  static const FirebaseOptions ios = FirebaseOptions(
+    apiKey: 'AIzaSyAuu5qjZZ9BUah73ihiKfZj8pwKRCElOlQ',
+    appId: '1:1053962346056:ios:1234567890abcdef',
+    messagingSenderId: '1053962346056',
+    projectId: 'myprojectfirebas-86a91',
+    storageBucket: 'myprojectfirebas-86a91.firebasestorage.app',
+    iosBundleId: 'com.example.flutterApplication1',
+  );
+
+  static const FirebaseOptions macos = FirebaseOptions(
+    apiKey: 'AIzaSyAuu5qjZZ9BUah73ihiKfZj8pwKRCElOlQ',
+    appId: '1:1053962346056:macos:1234567890abcdef',
+    messagingSenderId: '1053962346056',
+    projectId: 'myprojectfirebas-86a91',
+    storageBucket: 'myprojectfirebas-86a91.firebasestorage.app',
+    iosBundleId: 'com.example.diecastStore',
+  );
+
+  static const FirebaseOptions windows = FirebaseOptions(
+    apiKey: 'AIzaSyAuu5qjZZ9BUah73ihiKfZj8pwKRCElOlQ',
+    appId: '1:1053962346056:windows:1234567890abcdef',
+    messagingSenderId: '1053962346056',
+    projectId: 'myprojectfirebas-86a91',
+    storageBucket: 'myprojectfirebas-86a91.firebasestorage.app',
+  );
 }
+
+// Helper untuk detect web platform
+const bool kIsWeb = bool.fromEnvironment('dart.library.js_util');
